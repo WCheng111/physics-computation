f120b45f9fefc99c6d9f5be830f70f6a2ba5e838
WCheng <89013930+WCheng111@users.noreply.github.com>
2023-07-14 20:49:24 +0800

    update

--- iron vortex/pertua.py
+++ iron vortex/pertua.py
@@ -1,1 +1,15 @@
-import numpy
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def E(x):
+    E=x+2
+    return E
+x=np.linspace(-2,2,4)
+Eng=[]
+for i in range(len(x)):
+    Eng.append(E(i))
+plt.plot(x,Eng)
+plt.show()
+print(x)
