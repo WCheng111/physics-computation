19d220825d8683c0c0c0d5d804e5ddef3fb94ed9
WCheng <89013930+WCheng111@users.noreply.github.com>
2023-09-08 21:22:23 +0800

    update

--- IronFianl/Band.py
+++ IronFianl/Band.py
@@ -9,7 +9,7 @@ M0=25
 B3=-10
 B=25
 A=4
-SOC=0
+SOC=30
 
 def H(kx,ky,SOC):
     H=np.array([[M0+2*B3*(1-math.cos(kx))+2*B*(1-math.cos(ky)), -A*(math.sin(kx)-1j*math.sin(ky)), -A*(math.sin(kx)-1j*math.sin(ky))],
@@ -51,29 +51,32 @@ for i in range(len(kx)):
 for i in range(len(kx)):
     E[i+len(kx),:]=calate(kx[i],0,SOC)[0:3]
 
-# x=np.linspace(-1,1,2*len(kx))*math.pi
-# plt.plot(x,E[:,0],label='band1',color='black')
-# plt.plot(x,E[:,1],label='band2',color='black')
-# plt.plot(x,E[:,2],label='band3',color='black')
+x=np.linspace(-1,1,2*len(kx))*math.pi
+plt.plot(x,E[:,0],label='band1',color='black')
+plt.plot(x,E[:,1],label='band2',color='black')
+plt.plot(x,E[:,2],label='band3',color='black')
 # plt.axvline(x=0, ymin=0, ymax=1.2, color='r', linestyle='-',linewidth=1)
-#
+
 # plt.xlabel('kx')
+
+
+
 # plt.title(f'soc={SOC}')
+plt.ylabel('Energy')
+
+
+# plt.plot(kxx,Ex[:,2],label='up band',color='red',linewidth=3)
+# plt.plot(kxx,Ex[:,1],label='middle band',color='green',linewidth=3)
+# plt.plot(kxx,Ex[:,0],label='bottom band',color='blue',linewidth=3)
+# plt.axvline(x=-math.pi, ymin=0, ymax=1.2, color='black', linestyle='--',linewidth=2)
+# plt.axvline(x=0, ymin=0, ymax=1.2, color='black', linestyle='--',linewidth=2)
+# plt.axvline(x=math.pi, ymin=0, ymax=1.2, color='black', linestyle='--',linewidth=2)
+# plt.legend(loc='upper center', bbox_to_anchor=(0.2, 1))
+# plt.xticks([])
+# plt.text(-math.pi, -32, r'$Z$', ha='center', fontsize=15)
+# plt.text(0, -32, r'$\Gamma$', ha='center', fontsize=15)
+# plt.text(math.pi, -32, r'$Z$', ha='center', fontsize=15)
 # plt.ylabel('Energy')
 
-
-plt.plot(kxx,Ex[:,2],label='up band',color='red',linewidth=3)
-plt.plot(kxx,Ex[:,1],label='middle band',color='green',linewidth=3)
-plt.plot(kxx,Ex[:,0],label='bottom band',color='blue',linewidth=3)
-plt.axvline(x=-math.pi, ymin=0, ymax=1.2, color='black', linestyle='--',linewidth=2)
-plt.axvline(x=0, ymin=0, ymax=1.2, color='black', linestyle='--',linewidth=2)
-plt.axvline(x=math.pi, ymin=0, ymax=1.2, color='black', linestyle='--',linewidth=2)
-plt.legend(loc='upper center', bbox_to_anchor=(0.2, 1))
-plt.xticks([])
-plt.text(-math.pi, -32, r'$Z$', ha='center', fontsize=15)
-plt.text(0, -32, r'$\Gamma$', ha='center', fontsize=15)
-plt.text(math.pi, -32, r'$Z$', ha='center', fontsize=15)
-plt.ylabel('Energy')
-#
 
 plt.show()
